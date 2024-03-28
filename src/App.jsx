0a2c99564d19f522dd50ac64eee286ebeb9aa7f7
Claire Song <claireyuan.song@gmail.com>
2024-03-28 10:18:31 +0100

    trying to create a conflict is fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 // import "./App.css"
 
 function App() {
-	return (
-		<>
-			<p>A1nd mine :) A11y Accessibility</p>
-			<h1>Heeellllllloooooo</h1>
-  
-   <p>This is my contribution</p>
-		</>
-	)
+  return (
+    <>
+      <p>A1nd mine :) A11y Accessibility</p>
+      <h1>Heeellllllloooooo</h1>
+
+      <p>This is my contribution</p>
+      <p>And now my second contribution!</p>
+    </>
+  );
 }
 
 export default App;
