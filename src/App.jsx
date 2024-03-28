719abbcfd0bdbc75bd9c12ad06b4a852e7cee41c
Flow-Fly <florian.aube@gmail.com>
2024-03-28 10:19:55 +0100

    Random additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import "./App.css"
+import IsThisAComponent from "online"
 
 function App() {
 	return (
 		<>
 			<p>A1nd mine :) A11y Accessibility</p>
 			<h1>Heeellllllloooooo</h1>
-  
-   <p>This is my contribution</p>
+
+			<p>This is my contribution</p>
+			<IsThisAComponent />
 		</>
 	)
 }
 
-export default App;
+export default App
